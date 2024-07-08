3af0afe076a2ae50bc19f37423be7bf2bd7e2f1c
CodeWithBablu <babasahebbhalekar8245@gmail.com>
2024-07-08 20:10:53 +0530

    v1.0 live: happy coding

--- src/components/Layout/Sidebar.tsx
+++ src/components/Layout/Sidebar.tsx
@@ -39,84 +39,85 @@ const Sidebar = ({ isDrawer, handleClose }: Props) => {
   };
 
   return (
-    <div className={`${isDrawer ? 'flex w-full' : 'hidden xl:flex w-[276px]'} mt-14 sticky top-14 text-sm flex-col gap-2 p-3 h-fit max-h-[calc(100dvh-7rem)] overflow-y-scroll shrink-0 rounded-xl`}>
+    <div className={`${isDrawer ? 'flex w-full' : 'hidden xl:flex w-[276px]'} mt-14 hover:overflow-y-scroll sticky top-14 text-sm flex-col gap-2 p-3 h-full max-h-[calc(100dvh-7rem)] overflow-y-hidden transition-all duration-300 ease-in shrink-0 rounded-xl`}>
 
-      <div className='flex flex-col gap-2'>
-        {/* <CreateCommunityModal open={open} handleClose={() => setOpen(false)} /> */}
+      <div>
+        <div className='flex flex-col gap-2'>
+          {/* <CreateCommunityModal open={open} handleClose={() => setOpen(false)} /> */}
 
-        <div onClick={() => { (handleClose && handleClose()); navigate("/") }} className='z-50 px-3 py-3 text-gray-300 hover:bg-zinc-900/70 rounded-lg cursor-pointer flex items-center gap-3'><RiHome2Line /> Home</div>
-        <div onClick={() => { dispatch(setCreateCommunityModelOpen(true)); (handleClose && handleClose()); }} className='px-3 py-3 text-gray-300 hover:bg-zinc-900/70 rounded-lg cursor-pointer flex items-center gap-3'><RiAddLine /> Create Community</div>
-        <div onClick={() => { (handleClose && handleClose()); scrollToTop(); toggleMenuOpen(); }} className='px-3 py-3 text-gray-300 hover:bg-zinc-900/70 rounded-lg cursor-pointer flex items-center gap-3'><RiReceiptLine /> Create Post</div>
+          <div onClick={() => { (handleClose && handleClose()); navigate("/") }} className='z-50 px-3 py-3 text-gray-300 hover:bg-zinc-900/70 rounded-lg cursor-pointer flex items-center gap-3'><RiHome2Line /> Home</div>
+          <div onClick={() => { dispatch(setCreateCommunityModelOpen(true)); (handleClose && handleClose()); }} className='px-3 py-3 text-gray-300 hover:bg-zinc-900/70 rounded-lg cursor-pointer flex items-center gap-3'><RiAddLine /> Create Community</div>
+          <div onClick={() => { (handleClose && handleClose()); scrollToTop(); toggleMenuOpen(); }} className='px-3 py-3 text-gray-300 hover:bg-zinc-900/70 rounded-lg cursor-pointer flex items-center gap-3'><RiReceiptLine /> Create Post</div>
+        </div>
+
+        <hr className='border-zinc-900 my-3' />
+
+        <Accordion defaultIndex={[0, 1, 2]} allowMultiple>
+
+          <AccordionItem border={'none'}>
+            <h2>
+              <AccordionButton padding={'12px 12px'} _hover={{ backgroundColor: '#1d4ed8', textColor: "gray.100" }} className='text-gray-500 font-medium hover:bg-zinc-900/70 rounded-lg'>
+                <div className="tracking-wide">
+                  MODERATING
+                </div>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel padding={'unset'} >
+              {mySnippets.filter(snippet => snippet.isModerator).map((snippet, index) => (
+                <Link to={`/h/${snippet.communityId}`} key={index} className='mt-2 flex items-center cursor-pointer gap-3 px-3 py-2 hover:bg-zinc-900/70 rounded-xl'>
+                  <img className='w-[30px] h-[30px] shadow-[0px_0px_10px_0px_rgba(225,225,225,0.3)] object-cover rounded-full' src={snippet.imageURL ? snippet.imageURL : '/profile.png'} alt="comm img" />
+                  <span className='text-gray-300 text-sm tracking-wide'>h/{truncateText(snippet.communityId, 15)}</span>
+                </Link>
+              ))}
+            </AccordionPanel>
+          </AccordionItem>
+
+          <hr className='border-zinc-900 my-3' />
+
+          <AccordionItem border={'none'}>
+            <h2>
+              <AccordionButton padding={'12px 12px'} _hover={{ backgroundColor: '#1d4ed8', textColor: "gray.100" }} className='text-gray-500 font-medium hover:bg-zinc-900/70 rounded-lg'>
+                <div className="tracking-wide">
+                  COMMUNITIES
+                </div>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel padding={'unset'}>
+              {mySnippets.filter(snippet => !snippet.isModerator).map((snippet, index) => (
+                <Link to={`/h/${snippet.communityId}`} key={index} className='mt-2 flex items-center cursor-pointer gap-3 px-3 py-2 hover:bg-zinc-900/70 rounded-xl'>
+                  <img className='w-[30px] h-[30px] shadow-[0px_0px_10px_0px_rgba(225,225,225,0.3)] object-cover rounded-full' src={snippet.imageURL ? snippet.imageURL : '/profile.png'} alt="comm img" />
+                  <span className='text-gray-300 text-sm tracking-wide'>h/{truncateText(snippet.communityId, 15)}</span>
+                </Link>
+              ))}
+            </AccordionPanel>
+          </AccordionItem>
+
+          <hr className='border-zinc-900 my-3' />
+
+          <AccordionItem border={'none'}>
+            <h2>
+              <AccordionButton padding={'12px 12px'} _hover={{ backgroundColor: '#1d4ed8', textColor: "gray.100" }} className='text-gray-500 font-medium hover:bg-zinc-900/70 rounded-lg'>
+                <div className="tracking-wide">
+                  RECENT
+                </div>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel padding={'unset'}>
+              {recentCommunities.map((snippet, index) => (
+                <Link to={`/h/${snippet.communityId}`} key={index} className='mt-2 flex items-center cursor-pointer gap-3 px-3 py-2 hover:bg-zinc-900/70 rounded-xl'>
+                  <img className='w-[30px] h-[30px] shadow-[0px_0px_10px_0px_rgba(225,225,225,0.3)] object-cover rounded-full' src={snippet.imageURL ? snippet.imageURL : '/profile.png'} alt="comm img" />
+                  <span className='text-gray-300 text-sm tracking-wide'>h/{truncateText(snippet.communityId, 15)}</span>
+                </Link>
+              ))}
+            </AccordionPanel>
+          </AccordionItem>
+
+        </Accordion>
+
       </div>
-
-      <hr className='border-zinc-900 my-3' />
-
-      <Accordion defaultIndex={[0, 1, 2]} allowMultiple>
-
-        <AccordionItem border={'none'}>
-          <h2>
-            <AccordionButton padding={'12px 12px'} _hover={{ backgroundColor: '#1d4ed8', textColor: "gray.100" }} className='text-gray-500 font-medium hover:bg-zinc-900/70 rounded-lg'>
-              <div className="tracking-wide">
-                MODERATING
-              </div>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel padding={'unset'} >
-            {mySnippets.filter(snippet => snippet.isModerator).map((snippet, index) => (
-              <Link to={`/h/${snippet.communityId}`} key={index} className='mt-2 flex items-center cursor-pointer gap-3 px-3 py-2 hover:bg-zinc-900/70 rounded-xl'>
-                <img className='w-[30px] h-[30px] shadow-[0px_0px_10px_0px_rgba(225,225,225,0.3)] object-cover rounded-full' src={snippet.imageURL ? snippet.imageURL : '/profile.png'} alt="comm img" />
-                <span className='text-gray-300 text-sm tracking-wide'>h/{truncateText(snippet.communityId, 15)}</span>
-              </Link>
-            ))}
-          </AccordionPanel>
-        </AccordionItem>
-
-        <hr className='border-zinc-900 my-3' />
-
-        <AccordionItem border={'none'}>
-          <h2>
-            <AccordionButton padding={'12px 12px'} _hover={{ backgroundColor: '#1d4ed8', textColor: "gray.100" }} className='text-gray-500 font-medium hover:bg-zinc-900/70 rounded-lg'>
-              <div className="tracking-wide">
-                COMMUNITIES
-              </div>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel padding={'unset'}>
-            {mySnippets.filter(snippet => !snippet.isModerator).map((snippet, index) => (
-              <Link to={`/h/${snippet.communityId}`} key={index} className='mt-2 flex items-center cursor-pointer gap-3 px-3 py-2 hover:bg-zinc-900/70 rounded-xl'>
-                <img className='w-[30px] h-[30px] shadow-[0px_0px_10px_0px_rgba(225,225,225,0.3)] object-cover rounded-full' src={snippet.imageURL ? snippet.imageURL : '/profile.png'} alt="comm img" />
-                <span className='text-gray-300 text-sm tracking-wide'>h/{truncateText(snippet.communityId, 15)}</span>
-              </Link>
-            ))}
-          </AccordionPanel>
-        </AccordionItem>
-
-        <hr className='border-zinc-900 my-3' />
-
-        <AccordionItem border={'none'}>
-          <h2>
-            <AccordionButton padding={'12px 12px'} _hover={{ backgroundColor: '#1d4ed8', textColor: "gray.100" }} className='text-gray-500 font-medium hover:bg-zinc-900/70 rounded-lg'>
-              <div className="tracking-wide">
-                RECENT
-              </div>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel padding={'unset'}>
-            {recentCommunities.map((snippet, index) => (
-              <Link to={`/h/${snippet.communityId}`} key={index} className='mt-2 flex items-center cursor-pointer gap-3 px-3 py-2 hover:bg-zinc-900/70 rounded-xl'>
-                <img className='w-[30px] h-[30px] shadow-[0px_0px_10px_0px_rgba(225,225,225,0.3)] object-cover rounded-full' src={snippet.imageURL ? snippet.imageURL : '/profile.png'} alt="comm img" />
-                <span className='text-gray-300 text-sm tracking-wide'>h/{truncateText(snippet.communityId, 15)}</span>
-              </Link>
-            ))}
-          </AccordionPanel>
-        </AccordionItem>
-
-      </Accordion>
-
-      {/* <div className='h-[1000px] bg-rose-100'></div> */}
     </div>
   )
 }
